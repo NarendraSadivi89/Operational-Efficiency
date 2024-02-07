9ee86dfcb57eae43508f0ff56842f853909800f9
jayakrishnaCgi <jayakrishna.patibandla@cgi.com>
2024-02-07 09:47:15 -0600

    Test Git

--- Snow_Chat.py
+++ Snow_Chat.py
@@ -8,7 +8,7 @@ def main():
     st.set_page_config(page_title="Snow Chat", page_icon="❄")
     st.write(css, unsafe_allow_html=True)
     st.header("Snow Chat ❄")
-
+#write logic
 
 if __name__ == '__main__':
     main()
