ff369fc03a949f92cd5bde06187d1b18f20ebcf4
michael.culpepper <michael.culpepper@cgi.com>
2024-02-09 08:31:09 -0600

    SNOW glide query records -> pandas df -> pandas df agent

--- Snow_Chat.py
+++ Snow_Chat.py
@@ -1,40 +1,28 @@
 import streamlit as st
 from dotenv import load_dotenv
-from Snow_Utils import handle_question, get_text_chunks, get_vectorstore, get_conversation_chain
+from Snow_Utils import provision_snow, handle_question
 from htmlTemplates import css
 
 
 def main():
     load_dotenv()
+
     st.set_page_config(page_title="Snow Chat", page_icon="❄")
     st.write(css, unsafe_allow_html=True)
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
+    # if "conversation" not in st.session_state:
+    #     st.session_state.conversation = None
+    #
+    # if "chat_history" not in st.session_state:
+    #     st.session_state.chat_history = None
 
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
-
-    st.session_state.provisioning = True
-    while st.session_state.provisioning:
-        # get the text chunks
-        text_chunks = get_text_chunks('data/snow/response.json')
-        st.write('✅ Finished getting chunks.')
-
-        # create vectorstore
-        st.write('Preparing embeddings...')
-        vectorstore = get_vectorstore(text_chunks)
-        st.write('✅ Embeddings completed.')
-
-        # create conversation chain
-        st.session_state.conversation = get_conversation_chain(vectorstore)
-
-        st.session_state.provisioning = False
+    agent = provision_snow()
 
     st.header("Snow Chat ❄")
-    question = st.text_input("Ask question from your domain:")
-    if question:
-        handle_question(question)
+    st.container()
+    if prompt := st.chat_input():
+        handle_question(agent, prompt)
+
 
 if __name__ == '__main__':
     main()
