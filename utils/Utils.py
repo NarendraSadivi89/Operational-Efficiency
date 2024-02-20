20b7cb2459cd8050afd2e9368adeca2e26cb1132
michael.culpepper <michael.culpepper@cgi.com>
2024-02-20 11:00:49 -0600

    snow kb tables & diagram add jira

--- utils/Utils.py
+++ utils/Utils.py
@@ -49,14 +49,14 @@ def provision():
 
 
 def provision_snow(llm):
-    client = pysnc.ServiceNowClient('https://cgigroupincdemo15.service-now.com', ('api_user', os.getenv('snow_pass')))
+    client = pysnc.ServiceNowClient(os.getenv('snow_url'), ('api_user', os.getenv('snow_pass')))
     table_list = ['task', 'incident', 'sys_user', 'sys_user_group', 'core_company', 'cmn_location', 'cmn_cost_center',
-                  'cmn_department',
-                  'problem', 'kb_knowledge', 'change_request', 'change_task', 'std_change_producer_version',
+                  'cmn_department', 'problem', 'wf_workflow', 'kb_knowledge_base', 'kb_category', 'kb_knowledge',
+                  'kb_feedback', 'change_request', 'change_task', 'std_change_producer_version',
                   'cmdb', 'cmdb_ci', 'cmdb_rel_ci', 'cmdb_ci_computer', 'cmdb_ci_database', 'cmdb_ci_service',
                   'cmdb_ci_storage_device', 'cmdb_class_info', 'alm_asset', 'cmdb_model']
     # 'incident_task',
-    # 'change_request_template','change_collision',
+    # 'change_request_template', 'change_collision',
     # cmdb_ci_network_host,
     # cmdb_ci_cloud_service_account
     # cmdb_ci_network_adapter
